8ef0328a4b1cba6f765b96f219c95b882bc7c633
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-09-06 18:43:40 -0700

    added manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,62 +1,83 @@
-const APP_PREFIX = "PWA-budget-tracker-";
-const VERSION = "version_01";
-const CACHE_NAME = APP_PREFIX + VERSION;
+const CACHE_NAME = "my-site-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v2";
 
 const FILES_TO_CACHE = [
-  "/", "./index.html", "./js/idb.js", "./js/indexedDB.js", "./css/style.css", "./icons/icon-72x72.png",
+  "/",
+  "./index.html",
+  "./js/idb.js",
+  "./js/index.js",
+  "./css/style.css",
+  "./icons/icon-72x72.png",
   "./icons/icon-96x96.png",
   "./icons/icon-128x128.png",
   "./icons/icon-144x144.png",
   "./icons/icon-152x152.png",
   "./icons/icon-192x192.png",
   "./icons/icon-384x384.png",
-  "./icons/icon-512x512.png"
+  "./icons/icon-512x512.png",
 ];
 
 self.addEventListener("install", function (e) {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
+    caches.open(DATA_CACHE_NAME).then(function (cache) {
       console.log("installing cache : " + CACHE_NAME);
       return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
-      let cacheKeepList = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX);
-      });
-      cacheKeepList.push(CACHE_NAME);
-
       return Promise.all(
-        keyList.map(function (key, i) {
-          if (cacheKeepList.indexOf(key) === -1) {
-            console.log("deleting cache : " + keyList[i]);
-            return caches.delete(keyList[i]);
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
           }
         })
       );
     })
   );
+  self.ClientRectList.claim();
 });
 
 self.addEventListener("fetch", function (e) {
-  console.log("fetch request: " + e.request.url);
+  if (e.request.url.includes("/api/")) {
+    e.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(e.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(e.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(e.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
   e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) {
-        // if cache is available, respond with cache
-        console.log("responding with cache: " + e.request.url);
-        return request;
-      } else {
-        // if there are no cache, try fetching request
-        console.log("file is not cached, fetching: " + e.request.url);
-        return fetch(e.request);
-      }
-      // You can omit if/else for console.log and put one line below like this too.
-      // return request || fetch(e.request)
+    fetch(e.request).catch(function () {
+      return caches.match(e.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (e.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
     })
   );
 });
